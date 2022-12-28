105139cab4d291b9c5a0d7cf73f0968d107b9251
Otis <osgbala@gmail.com>
2022-12-28 19:50:32 +0000

    write migration for contact message and message delivery report model

---
+++ database/migrations/2022_12_24_143920_create_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstName')->nullable();
+            $table->string('lastName')->nullable();
+            $table->string('sex')->nullable();
+            $table->string('age')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
---
+++ database/migrations/2022_12_24_145017_create_message_delivery_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message_delivery_reports', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInt('message_id');
+            $table->unsignedInt('contact_id');
+            $table->smallInt('deliveryStatus');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message_delivery_reports');
+    }
+};
