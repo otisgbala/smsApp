be5c72a6ea2873cc445d26f6bb4146370db6e7d7
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2023-01-06 20:25:42 +0000

    Integrated SMS infobip in Message Controller

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -9,12 +9,46 @@ class MessageController extends Controller
 {
     //
     public function sendSMS (Request $request) {
-        //test send sms here .
+        
         $recipient = $request->get('recipient');
         $sender = $request->get('sender');
         $message = $request->get('message');
 
-        
+        $baseUrl = env('INFOBIP_BASE_URL');
+        $authorization = env('INFOBIP_AUTHORIZATION');
+        $response = Http::withHeaders([
+            'Authorization' => "App $authorization",
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json'
+        ])->post($baseUrl.'/sms/2/text/advanced', [
+            'messages' => [
+                    'from' => $sender,
+                    'destinations' => [
+                        [
+                            'to' => $recipient
+                        ]
+                    ],
+                    'text' => $message
+                ]
+        ]);
+
+        if ($response->successful()) {
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Message sent successfully',
+                'data' => $response->json()
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Message not sent',
+                'data' => $response->json()
+            ], 500);
+        }
+
 
     }
+
+    
 }
+
